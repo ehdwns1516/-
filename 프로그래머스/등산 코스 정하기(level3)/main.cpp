bafb70a773f08a5a9935e0a85dd643f98e79d4a0
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-09-28 18:35:08 +0900

    프로그래머스: 등산 코스 정하기(level3).cpp prim 알고리즘으로 MST 만드는 풀이 방법

--- 프로그래머스/등산 코스 정하기(level3)/main.cpp
+++ 프로그래머스/등산 코스 정하기(level3)/main.cpp
@@ -1,3 +1,4 @@
+// kruskal으로 MST 만든 것
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -102,6 +103,104 @@ vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector
     
     answer.push_back(top_number);
     answer.push_back(MIN);
+    
+    return answer;
+}
+
+// prim으로 MST 만든 것
+#include <string>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <tuple>
+#include <iostream>
+
+using namespace std;
+
+unordered_map<int, vector<pair<int, int>>> MST;
+unordered_map<int, vector<pair<int, int>>> edges;
+unordered_set<int> visited;
+unordered_set<int> is_gate;
+unordered_set<int> is_summit;
+
+int min_cost = 210000000;
+int min_summit = 0;
+
+void findCourse(int before, int curr, int cost){
+    if(is_summit.find(curr) != is_summit.end()){
+        if(min_cost > cost){
+            min_cost = cost;
+            min_summit = curr;
+        }
+        else if(min_cost == cost){
+            if(min_summit > curr)
+                min_summit = curr;
+        }
+        return;
+    }
+    
+    for(int i = 0; i < MST[curr].size(); i++){
+        auto [next_cost, next] = MST[curr][i];
+        if(next == before)
+            continue;
+        if(is_gate.find(next) != is_gate.end())
+            continue;
+        if(cost < next_cost)
+            findCourse(curr, next, next_cost);
+        else
+            findCourse(curr, next, cost);
+    }
+}
+
+vector<int> solution(int n, vector<vector<int>> paths, vector<int> gates, vector<int> summits) {
+    vector<int> answer;
+    
+    for(auto p : paths){
+        edges[p[0]].push_back({p[2], p[1]});
+        edges[p[1]].push_back({p[2], p[0]});
+    }
+    
+    for(int g : gates){
+        is_gate.insert(g);
+    }
+    
+    for(int s : summits){
+        is_summit.insert(s);
+    }
+    
+    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> PQ;
+
+    for(int i = 0; i < edges[1].size(); i++){
+        int curr = 1;
+        auto [cost, next] = edges[1][i];
+        PQ.push({cost, 1, next});
+    }
+    visited.insert(1);
+    
+    while(!PQ.empty()){
+        auto [c, before, curr] = PQ.top();
+        PQ.pop();
+        if(visited.find(curr) != visited.end())
+            continue;
+        visited.insert(curr);
+        MST[curr].push_back({c, before});
+        MST[before].push_back({c, curr});
+        
+        for(int i = 0; i < edges[curr].size(); i++){
+            auto [cost, next] = edges[curr][i];
+            if(visited.find(next) == visited.end()){
+                PQ.push({cost, curr, next});
+            }
+        }
+    }
+        
+    for(auto iter = is_gate.begin(); iter != is_gate.end(); iter++){
+        findCourse(0, *iter, 0);
+    }
+    
+    answer.push_back(min_summit);
+    answer.push_back(min_cost);
     
     return answer;
 }
